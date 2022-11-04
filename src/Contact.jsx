9f42d39b824c3fcb9ad0248e8fb4721ed482de5d
Wonuola <wonuolalonge@gmail.com>
2022-11-04 17:20:40 +0100

    feat: updated the form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -22,21 +22,22 @@ function Contact() {
         <h1 className='header'>Contact Form</h1>
         <form className='form' onSubmit={handleSubmit}>
         <label htmlFor="text">First Name</label>
-        <input type="text"  value={input.username} onChange={handleChange}  id='first_name'/>
+        <input type="text"  value={input.username} onChange={handleChange}  placeholder="Enter your first name" id='first_name'/>
 
         <label htmlFor="text">Last name</label>
-        <input type="text" id='last_name' value={input.username} onChange={handleChange} />
+        <input type="text" id='last_name' value={input.username} onChange={handleChange} placeholder="Enter your last name" />
 
         <label htmlFor="email">Email</label>
-        <input type="email"  id='email'/>
+        <input type="email" placeholder="your@example.com" id='email'/>
 
         <label htmlFor="text">Send me a message</label>
-        <textarea name="message" id="message" value={textnote} onChange={handleChanger
+        <textarea name="message" id="message" placeholder="Send me a message and I'll reply as soon as possible..." value={textnote} onChange={handleChanger
         } cols="30" rows="10"></textarea>
 
+<div><input type="checkbox" name="checkbox" id="checkbox" /> You agree to providing your data to {input.username} who may contact you</div>
         
         <div className='button'>
-        <button  value="Submit"  id='btn__submit' className='button-tag'>Submit</button>
+        <button  value="Submit"  id='btn__submit' className='button-tag'>Send a message</button>
         </div>
         </form>
     </div>
