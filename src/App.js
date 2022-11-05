47dfd14216949365e7e07aed85758c1b3c84b678
Wonuola <wonuolalonge@gmail.com>
2022-11-05 16:56:39 +0100

    fix: a bug fix

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { Route, Routes } from "react-router-dom";
 import Contact from './Contact';
 import Main from "./Main";
 
+
 function App() {
 
   return (
     <Routes>
     <Route path="/" element={<Main />}/>
     <Route path="/contact" element={<Contact />}/>
- 
+
     </Routes>
   );
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,96 +2,99 @@ import React from "react";
 import { useState } from "react";
 import Zuri from "./tools/zuri.svg";
 import IFG from "./tools/i4g.svg";
+import { Link } from "react-router-dom";
 
-function Contact() {
-  const [input, setInput] = useState(" ");
-  const [textnote, setTextNote] = useState(" ");
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInput((values) => ({ ...values, [name]: value }));
-  };
-  const handleChanger = (event) => {
-    setTextNote(event.target.value);
-  };
-  const handleSubmit = (e) => {
-    if (!this.validate() || input === null || input === "") {
-      e.preventDefault();
-    }
-  };
 
-  return (
-    <div className="form">
-      <div>
-        <h1 className="header">Contact Me</h1>
-        <p>Hi, there contact me toask about anything you have in mind</p>
-      </div>
-      <form className="form-details" onSubmit={handleSubmit}>
-        <div className="names">
-          <div>
-            <label htmlFor="text">First Name</label>
-            <br />
-            <input
-              type="text"
-              value={input.username}
-              onChange={handleChange}
-              placeholder="Enter your first name"
-              id="first_name"
-            />
+ export default function Contact() {
+
+  
+    return (
+      <div className="form">
+        <div>
+          <h1 className="header">Contact Me</h1>
+          <p>Hi, there contact me to ask about anything you have in mind</p>
+        </div>
+        <form>
+          <div className="names">
+            <div>
+              <label htmlFor="text">First Name</label>
+              <br />
+              <input
+                type="text"
+                name="fullName"
+                placeholder="Enter your first name"
+                id="first_name"
+                required
+              />
+
+            </div>
+
+            <div>
+              <label htmlFor="text">Last name</label>
+              <br />
+              <input
+                type="text"
+                name="fullName"
+                id="last_name"
+                placeholder="Enter your last name"
+                required
+                  
+              />{" "}
+             
+            </div>
           </div>
 
-          <div>
-            <label htmlFor="text">Last name</label>
-            <br />
-            <input
-              type="text"
-              id="last_name"
-              value={input.username}
-              onChange={handleChange}
-              placeholder="Enter your last name"
-            />
-          </div>
-        </div>
+    
+<label htmlFor="email">Email</label>
+<br />
+          <input
+            type="email"
+            placeholder="yourname@email.com"
+            id="email"
+            required
+          />
+<br/>
+         
 
-        <label htmlFor="email">Email</label>
-        <input type="email" placeholder="yourname@email.com" id="email" />
-
-        <label htmlFor="text">Send me a message</label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder="Send me a message and I'll reply as soon as possible..."
-          value={textnote}
-          onChange={handleChanger}
-          cols="30"
-          rows="10"
-        ></textarea>
-
-        <div className="check">
-          <input type="checkbox" name="checkbox" id="checkbox" /> <p>You agree to
-          providing your data to {input.username} who may contact you</p>
-        </div>
-
-        <div className="button">
-          <button value="Submit" id="btn__submit" className="button-tag">
-            Send a message
-          </button>
-        </div>
-      </form>
-      <footer>
-         <hr />
-         <div className="footer">
-           <a href="https://training.zuri.team/" className="IFG">
-             <img src={IFG} alt="IFG" />
-           </a>
-           <p>HNG Internship 9 Frontend Task</p>
-           <a href="/" className="Zuri">
-             <img src={Zuri} alt="zuri" />
-           </a>
+         <div className="area">
+         <label htmlFor="text">Send me a message</label>
+          <textarea
+            name="message"
+            id="message"
+            type="text"
+            placeholder="Send me a message and I'll reply as soon as possible..."
+            
+            cols="30"
+            rows="10"
+            required></textarea>
          </div>
-       </footer>
-    </div>
-  );
-}
 
-export default Contact;
+          <div className="check">
+            <input type="checkbox" name="checkbox" id="checkbox" required/>{" "}
+            <p>
+              You agree to providing your data to Wonuola who may contact you
+            </p>
+          </div>
+          <Link to='/input'>Next</Link>
+          <input type="submit" value="Submit" id="btn__submit"
+              className="button-tag submit" />
+       
+        </form>
+        <footer>
+          <hr />
+          <div className="footer">
+            <a href="https://training.zuri.team/" className="IFG">
+              <img src={IFG} alt="IFG" />
+            </a>
+            <p>HNG Internship 9 Frontend Task</p>
+            <a href="/" className="Zuri">
+              <img src={Zuri} alt="zuri" />
+            </a>
+          </div>
+        </footer>
+      </div>
+    )
+    
+  
+  }
+
